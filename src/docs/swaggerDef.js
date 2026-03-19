6120dd21e3ef8e387df2642ae06d6f7db307ebed
s43khu <s43khu@gmail.com>
2026-03-19 10:35:35 +0530

    chores: update project references and documentation; change repository links, modify README, and remove unused middleware

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -4,19 +4,16 @@ const config = require("../config/config");
 const swaggerDef = {
   openapi: "3.0.0",
   info: {
-    title: "node-express-boilerplate API documentation",
+    title: "react-vite-ts-boilerplate API documentation",
     version,
     license: {
       name: "MIT",
-      url: "https://github.com/hagopj13/node-express-boilerplate/blob/master/LICENSE",
+      url: "https://github.com/sh3xu/react-vite-ts-boilerplate/blob/main/LICENSE",
     },
   },
   servers: [
     {
       url: `http://localhost:${config.port}/v1`,
-    },
-    {
-      url: "http://vipankumar.in:4016/v1",
     },
   ],
 };
